74334a2769c886af52844da950ec68a4d158aac2
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-02-28 20:42:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,44 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import streamlit_authenticator as stauth
 import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
 
-"""
-# Welcome to Streamlit!yes
+with open('config.yaml') as file: #opening data file with user information
+    config = yaml.load(file, Loader=SafeLoader)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+authenticator = stauth.Authenticate( #setting up cookies
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-"""
-#create something
-"""
-def login():
-    username = input("Username: ")
-    password = input("Password: ")
+authenticator.login()
 
-    # You can replace the condition with your own authentication logic
-    if username == "admin" and password == "password":
-        print("Login successful!")
-    else:
-        print("Invalid credentials.")
+if st.session_state["authentication_status"]: #if the user is authenticated currently
+    authenticator.logout() #logout button
+elif st.session_state["authentication_status"] is False: # if incorrect password/username inputed when clicking login
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None: #if fields login fields are empty
+    st.warning('Please enter your username and password')
 
-login()
+if st.session_state["authentication_status"]: #if the user is authenticated currently
+    #do stuff here
+    st.write(f'Welcome *{st.session_state["name"]}* to comp 370 project')
+    st.title("Temperory header")
+    st.write('your username is: ' + st.session_state["username"])
+    st.write('your email is: '+config['credentials']['usernames'][st.session_state["username"]]['email'])
+    st.write('you currently have: '+str(config['credentials']['usernames'][st.session_state["username"]]['points']) + ' points')
+
+
+else: #registration for the website
+    try:
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(preauthorization=False)
+        if email_of_registered_user:
+            st.success('User registered successfully')
+    except Exception as e:
+        st.error(e)
+
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
